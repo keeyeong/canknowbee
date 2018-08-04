ce7d734d7c7564a853a9391842499de574e4de80
keeyeong <keeyeong.tan@gmail.com>
2018-08-04 21:31:35 +0200

    refactoring for readability

--- levenshtein_distance/templatetags/index_minus_one.py
+++ levenshtein_distance/templatetags/index_minus_one.py
@@ -4,5 +4,6 @@ register = template.Library()
 
 
 @register.filter
-def index(source_list, i):
+def index_minus_one(source_list, i):
+    """Returns the item at i-1 from source_list"""
     return source_list[int(i) - 1]
