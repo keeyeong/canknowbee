bf21a9f2ed01fda2f7e2d34db7694d3f3f174156
keeyeong <keeyeong.tan@gmail.com>
2018-08-04 13:09:23 +0200

    twitterscraper version

--- main/views.py
+++ main/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from twitterscraper import query
+
+import logging
+
 
 class MainView(TemplateView):
+
     template_name = "main/index.html"
+    logger = logging.getLogger(__name__)
+
 
     def get(self, request):
         context = {
@@ -12,7 +19,8 @@ class MainView(TemplateView):
         return render(request, 'main/index.html', context)
 
     def post(self, request):
+        tweets = query.query_single_page(request.POST['content'])[0]
         context = {
-            'results': [request.POST['content']]
+            'results': tweets
         }
         return render(request, 'main/index.html', context)
